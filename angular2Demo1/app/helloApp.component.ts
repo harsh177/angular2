83de4218b1c6d1ff71360fbab113997926b3077a
harsh177 <sharmaharsh177@gmail.com>
2017-05-13 20:45:50 +0530

    first init setup

---
+++ angular2Demo1/app/helloApp.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "angular2/core";
+
+@Component({selector:'hello-app',template:'<h2>Hello First App !!!!</h2>'})
+
+export  class HelloAppComponent{
+
+}
---
+++ angular2Demo1/app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from "angular2/platform/browser"
+import {HelloAppComponent} from "./helloApp.component"
+
+bootstrap(HelloAppComponent);
