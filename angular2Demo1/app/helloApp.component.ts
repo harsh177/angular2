44e6ba8ccef381e846f77d6209ce5653a4337008
harsh177 <sharmaharsh177@gmail.com>
2017-05-15 22:45:09 +0530

    delete old angular2 app

--- angular2Demo1/app/helloApp.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from "angular2/core";
-
-@Component({selector:'hello-app',template:'<h2>Hello First App !!!!</h2>'})
-
-export  class HelloAppComponent{
-
-}
--- angular2Demo1/app/main.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap} from "angular2/platform/browser"
-import {HelloAppComponent} from "./helloApp.component"
-
-bootstrap(HelloAppComponent);
